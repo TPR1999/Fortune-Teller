e42b1607611421541cacb789d346948362a534c4
TPR1999 <rickman.tanner@gmail.com>
2022-11-16 16:28:31 -0800

    completed Javascript fortune teller

---
+++ dailyMotivation.js
@@ -0,0 +1,10 @@
+const motivationFactory = (string1, string2) => {
+    return string1 + string2;
+}
+const string2 = () => {
+    const phrases = ['are gonna kill it!', 'are capable!', 'have to keep going!', 'will never give up!']
+    return phrases[Math.floor(math.random() * 4)];
+}
+string1 = 'You';
+
+console.log(string2);
