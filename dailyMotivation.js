4bd936e3ffb2cda3c3ba693e5217ae1b16f88ae7
TPR1999 <rickman.tanner@gmail.com>
2022-11-16 17:01:20 -0800

    correct fortune teller.

--- dailyMotivation.js
+++ dailyMotivation.js
@@ -1,10 +1,31 @@
-const motivationFactory = (string1, string2) => {
-    return string1 + string2;
+function randomNum(num){
+   return Math.floor(Math.random() * num);
 }
-const string2 = () => {
-    const phrases = ['are gonna kill it!', 'are capable!', 'have to keep going!', 'will never give up!']
-    return phrases[Math.floor(math.random() * 4)];
-}
-string1 = 'You';
 
-console.log(string2);
+
+const fortunes = {
+    spiritAnimal: ["Frog", "Pony", "Coy", "Ardvark"],
+    luck: ["You will see great wisdom.", "someone around you will see grear fortune.", "you will be blessed with great clarity.", "you will stumble upon a curiosity."],
+    toDo: ["buy a lottery ticket.", "be aware of your surroundings.", "belax and enjoy the day.", "see the beauty in the struggle."]
+};
+let myFortune = [];
+
+for(let prop in fortunes){
+    let fortuneGen = randomNum(fortunes[prop].length)
+    switch(prop){
+        case 'spiritAnimal':
+            myFortune.push(`Today your soul contains the spirit of the ${fortunes[prop][fortuneGen]}`)
+        break
+        case 'luck':
+            myFortune.push(`Upon this day ${fortunes[prop][fortuneGen]}`)
+        break
+        case 'toDo':
+            myFortune.push(`You should ${fortunes[prop][fortuneGen]}`)
+        break
+    }
+    
+}
+console.log(myFortune);
+
+
+ 
